46653a58db3210a14a9943c85a926c21ae456233
guimard <guimard@1dbb9719-a921-0410-b57f-c3a383c2c641>
2010-12-08 15:24:20 +0000

    * With Openldap 2.4.23, description must be ascii (base64 instead of Storable)  * Add missing files for Apache::Session::Browseable::LDAP #243
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/lemonldap/trunk/contribs/apache-session-ldap@1905 1dbb9719-a921-0410-b57f-c3a383c2c641

--- lib/Apache/Session/LDAP.pm
+++ lib/Apache/Session/LDAP.pm
@@ -10,7 +10,7 @@ use Apache::Session;
 use Apache::Session::Lock::Null;
 use Apache::Session::Store::LDAP;
 use Apache::Session::Generate::MD5;
-use Apache::Session::Serialize::Storable;
+use Apache::Session::Serialize::Base64;
 
 sub populate {
     my $self = shift;
@@ -19,8 +19,8 @@ sub populate {
     $self->{lock_manager} = new Apache::Session::Lock::Null $self;
     $self->{generate}     = \&Apache::Session::Generate::MD5::generate;
     $self->{validate}     = \&Apache::Session::Generate::MD5::validate;
-    $self->{serialize}    = \&Apache::Session::Serialize::Storable::serialize;
-    $self->{unserialize}  = \&Apache::Session::Serialize::Storable::unserialize;
+    $self->{serialize}    = \&Apache::Session::Serialize::Base64::serialize;
+    $self->{unserialize}  = \&Apache::Session::Serialize::Base64::unserialize;
 
     return $self;
 }
